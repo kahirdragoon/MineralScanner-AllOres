79d3abaec563afe56d8780b44a0054603a0f2155
KahirDragoon <markus.deeken@gmail.com>
2024-02-18 15:25:05 +0100

    Add mod files
    Signed-off-by: KahirDragoon <markus.deeken@gmail.com>

---
+++ Source/Initialize.cs
@@ -0,0 +1,24 @@
+﻿using RimWorld;
+using System.Linq;
+using Verse;
+
+namespace RCP_Core
+{
+    [StaticConstructorOnStartup]
+    public class Initialize
+    {
+        private const string ModName = "MineralScannerAllOres";
+
+        static Initialize()
+        {
+            var preciousLump = DefDatabase<SitePartDef>.GetNamed(GenStepDefOf.PreciousLump.ToString());
+            var genStep = preciousLump.ExtraGenSteps.First(egs => egs.genStep is GenStep_PreciousLump).genStep as GenStep_PreciousLump;
+            
+            var minableOres = DefDatabase<ThingDef>
+                .AllDefsListForReading
+                .Where(def => def.building != null && def.building.isResourceRock && def.building.mineableScatterCommonality > 0 && !genStep.mineables.Contains(def));
+
+            genStep.mineables.AddRange(minableOres);
+        }
+    }
+}
